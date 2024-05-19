a7d6d608afc89fa4ff635a697757b69dc3724bf4
Nicolas Santillan <dev.nicomusic@gmail.com>
2024-05-19 07:51:25 -0500

    Add Person Service

---
+++ src/main/java/com/person/service/impl/PersonServiceImpl.java
@@ -0,0 +1,37 @@
+package com.person.service.impl;
+
+import com.person.entities.Person;
+import com.person.persistence.IPersonDAO;
+import com.person.service.IPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonServiceImpl implements IPersonService {
+
+    @Autowired
+    private IPersonDAO personDAO;
+
+    @Override
+    public List<Person> findAll() {
+        return personDAO.findAll();
+    }
+
+    @Override
+    public Optional<Person> findById(Long id) {
+        return personDAO.findById(id);
+    }
+
+    @Override
+    public void save(Person person) {
+        personDAO.save(person);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        personDAO.deleteById(id);
+    }
+}
