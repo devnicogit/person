aae057f510ca9f9cdf334d48562b1464edeb32d7
Nicolas Santillan <dev.nicomusic@gmail.com>
2024-05-19 10:47:19 -0500

    Changes applied

---
+++ src/main/java/com/person/controller/PersonController.java
@@ -0,0 +1,26 @@
+package com.person.controller;
+
+import com.person.entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/person")
+public class PersonController {
+    @Autowired
+    private IPersonService personService;
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        Optional<Person> personOptional = personService.findById(id);
+        if(personOptional.isPresent()){
+
+        }
+    }
+}
