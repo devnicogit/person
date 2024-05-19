e70a75036cec9deb7139fbae5daf9915058dc031
Nicolas Santillan <dev.nicomusic@gmail.com>
2024-05-18 19:49:39 -0500

    Edit Person Repository

---
+++ src/main/java/com/person/controller/PersonController.java
@@ -0,0 +1,13 @@
+package com.person.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class PersonController {
+
+    @Autowired
+    private 
+}
